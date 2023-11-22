05c4c2e095fdf16da2bae7693793b98de51261c3
Arosha Rukshan <rukshanarosha@gmail.com>
2023-11-22 22:57:07 +0530

    Rename Method

--- src/PictureFrame.java
+++ src/PictureFrame.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class PictureFrame {
   public int[] reroll = null;
-  Aardvark master = null;
+  Main master = null;
 
   class DominoPanel extends JPanel {
     private static final long serialVersionUID = 4190229282411119364L;
@@ -64,10 +64,12 @@ public class PictureFrame {
     }
 
 
-    void drawFun(Graphics g, int x, int y, int n,
-        FontMetrics fm = g.getFontMetrics();
-        String txt = Integer.toString(n);
-        g.drawString(txt, x - fm.stringWidth(txt) / 2, y + fm.getMaxAscent() / 2);
+    void drawDigitGivenDominoCentre(Graphics g, int x, int y, int diameter, int n,
+        Color c) {
+      int radius = diameter / 2;
+      g.setColor(c);
+      // g.drawOval(x - radius, y - radius, diameter, diameter);
+      drawFun(g,n,x,y);
     }
 
     void fillDigitGivenCentre(Graphics g, int x, int y, int diameter, int n) {
