0d2ea6c89e5e360041da5ab89921b49da7f702b3
Arosha Rukshan <rukshanarosha@gmail.com>
2023-11-22 18:41:07 +0530

    Removed the unused functions

--- src/ConnectionGenius.java
+++ src/ConnectionGenius.java
@@ -1,9 +1,6 @@
 package base;
 import java.net.InetAddress;
-/**
- * @author Kevan Buckley, maintained by __student
- * @version 2.0, 2014
- */
+
 
 public class ConnectionGenius {
 
@@ -14,22 +11,7 @@ public class ConnectionGenius {
   }
 
   public void fireUpGame() {
-    downloadWebVersion();
-    connectToWebService();
-    awayWeGo();
-  }
-  
-  public void downloadWebVersion(){
-    System.out.println("Getting specialised web version.");
-    System.out.println("Wait a couple of moments");  
-  }
-  
-  public void connectToWebService() {
-    System.out.println("Connecting");
-  }
-  
-  public void awayWeGo(){
-    System.out.println("Ready to play");
+  System.out.println("FireUpGame function invoked!");
   }
 
 }
